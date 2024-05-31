846f1ca702f8846b3309e0721aaaa87b77a68d24
Енакаев Алексей <enakaev.aleksey@yandex.ru>
2024-05-31 14:10:06 +0300

    Реализована основная ф-ция приложения (загрузка модуля и вызов методов)

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,45 @@
 #include <stdexcept>
 #include <iostream>
 
-#include <boost/core/ignore_unused.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/dll/import.hpp>
+#include <boost/function.hpp>
 #include <boost/exception/diagnostic_information.hpp>
+
+#include <module/mathop.h>
 
 
 int main( int argc, char** argv )
 {
-     boost::ignore_unused( argc, argv );
      try
      {
+          if( argc < 2 )
+          {
+               BOOST_THROW_EXCEPTION(
+                    std::invalid_argument{ "too few parameters: no module path specified" } );
+          }
 
+          boost::dll::fs::path modulePath{ argv[ 1 ] };
+          std::cout << "Loading module: " << modulePath.string() << '\n';
+
+          using IMathOpPtr = boost::shared_ptr< using_modules::module::IMathOp >;
+          /// Сигнатура функции-создателя модуля
+          using ModuleCreatorSgn = IMathOpPtr();
+          /// Функтор для создания модуля
+          using ModuleCreatorFn = boost::function< ModuleCreatorSgn >;
+
+          ModuleCreatorFn creator = boost::dll::import_alias< ModuleCreatorSgn >(
+               modulePath
+               , "createModule"
+               , boost::dll::load_mode::append_decorations
+               );
+
+          auto module = creator();
+
+          int a = 7, b = 9;
+          std::cout
+               << "Name: " << module->name() << '\n'
+               << "Op for " << a << " and " << b << ": " << module->action( a, b ) << '\n';
      }
      catch( const std::exception& e )
      {
